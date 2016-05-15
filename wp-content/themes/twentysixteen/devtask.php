011de87d2ec15dcb8f7d0348cc6d744b6e527675
Tod Cribb <todcribb@Samanthas-iMac.local>
2016-05-15 14:10:30 -0400

    updates to responsive

--- wp-content/themes/twentysixteen/devtask.php
+++ wp-content/themes/twentysixteen/devtask.php
@@ -32,7 +32,7 @@ Template Name: Front End Dev Task
     <script src="js/transition.js"></script>
 
     <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
-    <link href='https://fonts.googleapis.com/css?family=PT+Serif' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans|Merriweather' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">
 
 
@@ -42,11 +42,11 @@ Template Name: Front End Dev Task
 <body>
 	<div class="container-fluid">
 		<div class="header container-fluid">	
-				
+				<div class="logo-cont col-xs-4 col-sm-4 col-md-3 col-lg-3">
 					<img src="/wp-content/assets/RMH-logo.jpg" class="header-logo" />
-				
+				</div>
 
-				<div class="main-nav ">
+				<div class="main-nav col-xs-8 col-sm-8 col-md-9 col-lg-9">
 					<a href="#" role="button">Your Impact</a>
 					<a href="#" role="button">Ways to Give</a>
 					<a href="#" role="button">How Your Gift is Used</a>
@@ -57,23 +57,29 @@ Template Name: Front End Dev Task
 
 		</div>
 		<div class="slider container-fluid">
-			<div class="slide-link-container">
+			<div class="row slide-link-container">
 				<div class="slide-img-link-bg"></div>
 				<div class="slide-link-text">Funding compassionate care</div>
 				<a href="#"><div class="slide-link-button"><span>READ BILL AND GERRY'S STORY</span></div></a>
 			</div>
 		</div>
+
 		<div class="reason-container container-fluid">
-			<?php $loop = new WP_Query( array( 'post_type' => 'reasons', 'posts_per_page' => 1, 'order' => 'ASC') ); ?>
-			<?php while ( $loop->have_posts() ) : $loop->the_post();?>
-				<div class="reason-title"><?php echo the_title(); ?></div>
-				<div class="reason-text"><?php echo the_content(); ?></div>
-			<?php endwhile; ?>
+			<div class="row col-lg-12">
+				<?php $loop = new WP_Query( array( 'post_type' => 'reasons', 'posts_per_page' => 1, 'order' => 'ASC') ); ?>
+				<?php while ( $loop->have_posts() ) : $loop->the_post();?>
+					<div class="reason-title"><?php echo the_title(); ?></div>
+					<div class="reason-text"><?php echo the_content(); ?></div>
+				<?php endwhile; ?>
+			</div>
 		</div>
-		<div class="impacts-container container-fluid">
-			<div class="container">
+		
+		<div class="impacts-container conatiner-fluid">
+			<div class="row">
 				<div class="impacts-title"> OUR IMPACT </div>
-				<?php $loop = new WP_Query( array( 'post_type' => 'impacts', 'posts_per_page' => 3, 'order' => 'ASC') ); ?>
+			</div>
+			<div class="row">
+					<?php $loop = new WP_Query( array( 'post_type' => 'impacts', 'posts_per_page' => 3, 'order' => 'ASC') ); ?>
 				<?php while ( $loop->have_posts() ) : $loop->the_post(); $impacturl = get_permalink();?>
 					<div class="impact-block col-sm-12 col-md-4 col-lg-4">
 						<div class="impact-number"><?php echo get_field('numbers'); ?></div>
@@ -85,11 +91,12 @@ Template Name: Front End Dev Task
 					</div>
 				<?php endwhile; ?>
 			</div>
-		</div><!-- .content-area -->
-		<div class="latest-blog container-fluid">
+		</div>
 
-			<?php $loop = new WP_Query( array( 'post_type' => 'post', 'posts_per_page' => 1, 'order' => 'DESC') ); ?>
-			<?php while ( $loop->have_posts() ) : $loop->the_post(); 
+		<div class="latest-blog container-fluid">
+			<div class="row">
+				<?php $loop = new WP_Query( array( 'post_type' => 'post', 'posts_per_page' => 1, 'order' => 'DESC') ); ?>
+				<?php while ( $loop->have_posts() ) : $loop->the_post(); 
 					$posturl = get_permalink(); ?>
 					<div class="post-image col-sm-12 col-md-6 col-lg-6">
 						<?php echo the_post_thumbnail() ?>
@@ -103,10 +110,10 @@ Template Name: Front End Dev Task
 						</div>
 						<div class="post-read-more"><a href="<?php echo $posturl; ?>">Read More</a></div>
 					</div>
-					
-			<?php endwhile; ?>
-
+				<?php endwhile; ?>
+			</div>
 		</div>
+
 		<div class="testimonial container-fluid">
 			<div class="testimonial-shade"></div>
 			<div class="testimonial-box">
@@ -118,27 +125,34 @@ Template Name: Front End Dev Task
 				<?php endwhile; ?>
 			</div>
 		</div>
+
 		<div class="footer container-fluid">
-			<div class="sm-footer">
-				<div class="left-links col-sm-12 col-md-6 col-lg-6">
+
+			<div class="row sm-footer">
+				<div class="left-links col-xs-12 col-sm-6 col-md-6 col-lg-6">
 					<a href="#" class="footer-link">Calendar</a>
 					<a href="#" class="footer-link">Style Guide</a>
 				</div>
-				<div class="social-links col-sm-12 col-md-6 col-lg-6">
-					<i class="fa fa-facebook-square" aria-hidden="true"></i>
-					<i class="fa fa-twitter-square" aria-hidden="true"></i>
-					<i class="fa fa-youtube-square" aria-hidden="true"></i>
-					<i class="search"><?php get_search_form( $echo = true ) ?></i>
+				<div class="social-links col-xs-12 col-sm-6 col-md-6 col-lg-6">
+					<div class="search">
+						<i class="search"><?php get_search_form( $echo = true ) ?></i>
+					</div>
+					<div class="social">
+						<i class="fa fa-facebook-square" aria-hidden="true"></i>
+						<i class="fa fa-twitter-square" aria-hidden="true"></i>
+						<i class="fa fa-youtube-square" aria-hidden="true"></i>
+					</div>
 				</div>
 			</div>
-			<div class="lg-footer">
-				<div class="inform col-sm-12 col-md-6 col-lg-6">
+
+			<div class="row lg-footer">
+				<div class="inform col-xs-12 col-sm-6 col-md-6 col-lg-6">
 					<div class="inform-title">STAY INFORMED</div>
 					<label class="email-label">Email Address<span>*</span></label>
 					<input class="email-input" />
 					<button class="submit-email">SUBMIT</button>
 				</div>
-				<div class="info col-sm-12 col-md-6 col-lg-6">
+				<div class="info col-xs-12 col-sm-6 col-md-6 col-lg-6">
 					<div class="loc-cont">
 						<div class="info-title">RMH FOUNDATION</div>
 						<div class="info-addr">2010 Health Campus Drive Harrisonburg, VA 22801</div>
